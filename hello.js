dda430a175f27f9d63e9b6069ba211d351e49b84
shukla34 <shreyasshukla257@gmail.com>
2025-09-02 22:22:40 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+console.log("hello");
+const name = "shreyas"
+console.log(name)
+{
+    let name = "shreyas"
+    console.log(name)
+}
+{
+    let name = "shreyas"
+    console.log(typeof name)
+}
